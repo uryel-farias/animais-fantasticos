d4245d680321fded00d25e08b9a570d9209697b2
Uryel Farias <uryel.farias@gmail.com>
2022-05-28 09:31:57 -0300

    navegação por tabs ok

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const linksInternos = document.querySelectorAll('a[haref^=#]');
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabConteudo = document.querySelectorAll(".js-tabconteudo section");
 
-function handleLink(event){
-    console.log(event);
+
+if(tabMenu.length && tabConteudo.length){
+
+    tabConteudo[0].classList.add("ativo");
+    function activeTab(index) {
+        tabConteudo.forEach((section) => {
+            section.classList.remove("ativo");
+        });
+        tabConteudo[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener("click", () => {
+            activeTab(index);
+        });
+    });
 }
-
-linksInternos.forEach((link) => {
-    link.addEventListener('click', handleLink);
-})

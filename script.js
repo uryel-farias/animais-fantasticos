2ddf352e122df2f709849c6c418ab5bfe74af09b
Uryel Farias <uryel.farias@gmail.com>
2022-05-28 11:18:01 -0300

    Adicionado accordeon list no FAQ

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabConteudo = document.querySelectorAll(".js-tabconteudo section");
+function initTabNav(){
+    const tabMenu = document.querySelectorAll(".js-tabmenu li");
+    const tabConteudo = document.querySelectorAll(".js-tabconteudo section");
 
 
-if(tabMenu.length && tabConteudo.length){
+    if(tabMenu.length && tabConteudo.length){
 
-    tabConteudo[0].classList.add("ativo");
-    function activeTab(index) {
-        tabConteudo.forEach((section) => {
-            section.classList.remove("ativo");
+        tabConteudo[0].classList.add("ativo");
+        function activeTab(index) {
+            tabConteudo.forEach((section) => {
+                section.classList.remove("ativo");
+            });
+            tabConteudo[index].classList.add("ativo");
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener("click", () => {
+                activeTab(index);
+            });
         });
-        tabConteudo[index].classList.add("ativo");
     }
-
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener("click", () => {
-            activeTab(index);
-        });
-    });
 }
+
+
+initTabNav();
+
+function initAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    
+    if(accordionList.length){
+        accordionList[0].classList.add('ativar-dd');
+        accordionList[0].nextElementSibling.classList.add('ativar-dd');
+
+
+
+        function activeAccordion() {
+            this.classList.toggle('ativar-dd');
+            this.nextElementSibling.classList.toggle('ativar-dd');
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        })
+    }
+}
+
+initAccordion();

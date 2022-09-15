80d77b01ab956d27a6d69177f99d594f47f0602d
Uryel Farias <uryel.farias@gmail.com>
2022-09-15 07:12:03 -0300

    Interatividade do modal crida
    função de abrir e fechar do modal foi implementada em um arquivo js.

---
+++ modules/modal.js
@@ -0,0 +1,29 @@
+export default function initModal() {
+    const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+    const botaoFechar = document.querySelector('[data-modal="fechar"]');
+    const containerModal = document.querySelector('[data-modal="container"]');
+    
+    if(botaoAbrir && botaoFechar && containerModal){
+    
+        function abrirModal(event){
+            event.preventDefault();
+            containerModal.classList.add('ativo');
+        }
+    
+        function fecharModal(event){
+            event.preventDefault();
+            containerModal.classList.remove('ativo');
+        }
+    
+        function clickForaModal(event) {
+            if(event.target === this){ //event.target é o container do modal e this é a referencia do click do mouse. 
+                fecharModal(event);
+            }
+        }
+    
+        botaoAbrir.addEventListener('click', abrirModal);
+        botaoFechar.addEventListener('click', fecharModal);
+        containerModal.addEventListener('click', clickForaModal);
+    }
+
+}
--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 import initScrollSuave from './modules/scroll-suave.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
+import initModal from './modules/modal.js';
 
 initScrollSuave();
 initAccordion();
 initTabNav();
+initModal();
 

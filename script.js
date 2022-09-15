570c519f34df8f984b715e8ddaf53459acd81e20
Uryel Farias <uryel.farias@gmail.com>
2022-09-15 08:37:41 -0300

    Criação do tooltip
    Criado a interação de passar o mouse sobre a imagem do mapa.

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+
 
 initScrollSuave();
 initAccordion();
 initTabNav();
 initModal();
-
+initTooltip();

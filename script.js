e9e53ddee7729e386d28039e1c41cb99e2fdf3eb
Uryel Farias <uryel.farias@gmail.com>
2022-11-01 09:42:13 -0300

    Arquivos js e css para o Dropdown-menu criados.

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
-
+import initDropdownMenu from './modules/dropdown-menu';
 
 initScrollSuave();
 initAccordion();
 initTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();

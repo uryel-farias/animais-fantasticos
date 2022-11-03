18a56d2e82caf1136962e7d96bb67500b51e72f2
Uryel Farias <uryel.farias@gmail.com>
2022-11-03 10:12:24 -0300

    função dropdown do menu SOBRE finalizada

--- modules/dropdown-menu.js
+++ modules/dropdown-menu.js
@@ -1,3 +1,16 @@
 export default function initDropdownMenu(){
-    
+
+}
+
+const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+dropdownMenus.forEach(menu => {
+    ['touchstart','click','hover'].forEach(userEvent => {
+        menu.addEventListener(userEvent, handleClick);
+    })
+})
+
+function handleClick(event) {
+    event.preventDefault();
+   this.classList.toggle('active');
 }
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAccordion();

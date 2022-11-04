790033181e484a1def3d4dbceb1766dde7efa064
Uryel Farias <uryel.farias@gmail.com>
2022-11-04 10:50:36 -0300

    Criado e finalizado o arquivo e a função clickoutside

--- modules/dropdown-menu.js
+++ modules/dropdown-menu.js
@@ -1,16 +1,23 @@
+import outsideClick from "./outsideClick.js";
+
 export default function initDropdownMenu(){
-
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');  // (1) seleciona o elemento 
+    
+    dropdownMenus.forEach(menu => {
+        ['touchstart','click'].forEach(userEvent => { // [... ...] quando que usar mais de um tipo de evento em uma linha. 
+            menu.addEventListener(userEvent, handleClick); // (2) ativa a função handleClick quando usar o click//hover ou touchstart(mobile)
+        });
+    });
+    
+    function handleClick(event) {
+        event.preventDefault(); // (3) Previne abrir uma nova janela
+        this.classList.add('active'); // (4) Adiciona active ao elemento selecionado (*li dropdown)
+        outsideClick(this, ['touchstart','click'], () => {  // (5) cria a função de click fora do submenu
+            this.classList.remove('active'); // (6) 
+        });
+    };
 }
 
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-dropdownMenus.forEach(menu => {
-    ['touchstart','click','hover'].forEach(userEvent => {
-        menu.addEventListener(userEvent, handleClick);
-    })
-})
 
-function handleClick(event) {
-    event.preventDefault();
-   this.classList.toggle('active');
-}
+

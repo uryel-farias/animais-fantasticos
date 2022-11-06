a27583848d63de030df92bac680c426493f64643
Uryel Farias <uryel.farias@gmail.com>
2022-11-06 11:51:24 -0300

    Comentários e início da seção Números

--- modules/menu-mobile.js
+++ modules/menu-mobile.js
@@ -18,7 +18,7 @@ export default function initMenuMobile() {
         
         eventos.forEach((userEvent) => {
             menuButtom.addEventListener(userEvent, openMenu);
-        
+            
         })
     }
 }

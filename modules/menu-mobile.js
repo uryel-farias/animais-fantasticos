a25356beca6e4e397d8dd81669112280a81a535a
Uryel Farias <uryel.farias@gmail.com>
2022-11-06 11:29:11 -0300

    Menu mobile implementado

---
+++ modules/menu-mobile.js
@@ -0,0 +1,24 @@
+import outsideClick from "./outsideClick.js";
+
+export default function initMenuMobile() {
+    
+    const menuButtom = document.querySelector('[data-menu="buttom"]');
+    const menuList = document.querySelector('[data-menu="list"]');
+    const eventos = ['click', 'touchstart'];
+    
+    if(menuButtom){
+        function openMenu(event) {
+            menuList.classList.add('active');
+            menuButtom.classList.add('active');
+            outsideClick(menuList, eventos, () => {
+                menuList.classList.remove('active');
+                menuButtom.classList.remove('active');
+            });
+        }
+        
+        eventos.forEach((userEvent) => {
+            menuButtom.addEventListener(userEvent, openMenu);
+        
+        })
+    }
+}
